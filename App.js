ce74e191ed6b706636a590bcef717d8dc1fd3b66
ofirshtrosberg <ofiri112@gmail.com>
2023-03-03 15:50:56 +0200

    basic bottom navigation

--- App.js
+++ App.js
@@ -1,26 +1,40 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Provider, Button } from "react-native-paper";
-import { theme } from "./src/core/theme";
+import * as React from "react";
+import { Text, View } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import HomePage from "./src/pages/HomePage";
+import LoginPage from "./src/pages/LoginPage";
+import RegisterPage from "./src/pages/RegisterPage";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <Provider theme={theme}>
-      <View style={styles.container}>
-        <Text>
-          hiiiii!!!
-        </Text>
-        <Button>click!</Button>
-      </View>
-    </Provider>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Home") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+            } else if (route.name === "Login") {
+              iconName = focused ? "ios-list" : "ios-list-outline";
+            } else if (route.name === "Register") {
+              iconName = focused ? "ios-list" : "ios-list-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#5F84A2",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen name="Login" component={LoginPage} />
+        <Tab.Screen name="Register" component={RegisterPage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:theme.colors.border,
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text, View } from "react-native";
+export default function LoginPage() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Login</Text>
+    </View>
+  );
+}
