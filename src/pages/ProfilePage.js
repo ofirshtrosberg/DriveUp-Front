003a590641cc00ebca9dee10fd427c560ceb4626
LidorDan <lidordanon30@cs.colman.ac.il>
2023-04-02 16:17:09 +0300

    Update ProfilePage.js

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -3,7 +3,17 @@ import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
 
+import HeaderLogout from "../components/HeaderLogout";
+// import { useNavigation } from '@react-navigation/native';
+
 export default function ProfilePage({ navigation }) {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: 'Home',
+      headerRight: () => <HeaderLogout />
+
+    });
+  }, [navigation]);
   return (
     <View style={styles.container}>
       <Text>ProfilePage</Text>
