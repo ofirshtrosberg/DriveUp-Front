028514d3bc9174f68588f92ea3d5ebd9bfe1b1ad
ofirshtrosberg <ofiri112@gmail.com>
2023-05-01 22:48:50 +0300

    taxi page

---
+++ src/pages/DriverRoutesOffersScreen.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
+const data = [
+  { orderNumber: "12345", numberOfPassengers: 3, estimatedProfit: 35 },
+  { orderNumber: "12346", numberOfPassengers: 4, estimatedProfit: 36 },
+  { orderNumber: "12347", numberOfPassengers: 5, estimatedProfit: 40 },
+];
+export default function DriverRoutesOffersPage({navigation}) {
+  return (
+    <View style={styles.container}>
+      {data.map((item) => (
+        <View
+          key={item.orderNumber}
+          style={{
+            borderBottomWidth: 1,
+            padding: 10,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <View style={{ flex: 1, justifyContent: "space-between" }}>
+            <Text style={{ marginBottom: 2 }}>
+              Order number:{item.orderNumber}
+            </Text>
+            <Text style={{ marginBottom: 2 }}>
+              Number of passengers:{item.numberOfPassengers}
+            </Text>
+            <Text style={{ marginBottom: 2 }}>
+              Estimated Profit:{item.estimatedProfit}$
+            </Text>
+          </View>
+          <View>
+            <Button
+              mode="contained"
+              buttonColor="#111"
+              style={styles.watchDetailsBtn}
+              onPress={() => {}}
+            >
+              Watch details
+            </Button>
+          </View>
+        </View>
+      ))}
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  watchDetailsBtn: {
+    width: 150,
+  },
+});
---
+++ src/pages/TaxiPage.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Text, View, StyleSheet, ActivityIndicator } from "react-native";
+import HeaderLogout from "../components/HeaderLogout";
+import { useNavigation } from "@react-navigation/native";
+import PassengerOrderTaxiPage from "./PassengerOrderTaxiPage";
+import DriverRoutesOffersPage from "./DriverRoutesOffersScreen";
+import { useFocusEffect } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getUserByEmail } from "../../AsyncStorageUsers";
+import { useState } from "react";
+export default function TaxiPage() {
+  const navigation = useNavigation();
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: "Order Taxi",
+      headerRight: () => <HeaderLogout />,
+    });
+  }, [navigation]);
+
+  const [plateNumber, setPlateNumber] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchUser = async () => {
+    try {
+      setIsLoading(true);
+      const value = await AsyncStorage.getItem("currentUserEmail");
+      if (value !== null && value !== "") {
+        const fetchedUser = await getUserByEmail(value);
+        setPlateNumber(fetchedUser.plate_number);
+      }
+    } catch (error) {
+      console.error("Error fetching user:", error);
+    } finally {
+      // setTimeout(() => {
+      setIsLoading(false);
+      // }, 3000);
+    }
+  };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      fetchUser();
+      return () => {};
+    }, [])
+  );
+
+  return (
+    <View style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#000" />
+      ) : plateNumber === "" ? (
+        <PassengerOrderTaxiPage />
+      ) : (
+        <DriverRoutesOffersPage />
+      )}
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  loading: {
+    alignSelf: "center",
+  },
+});
