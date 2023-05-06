2e41467a312a9fbf9b0bb1d3a9e13f553884038a
ofirshtrosberg <ofiri112@gmail.com>
2023-05-07 00:54:43 +0300

    map with 2 markers

--- src/pages/PassengerOrederOnMap.js
+++ src/pages/PassengerOrederOnMap.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { Text, View, StyleSheet, ActivityIndicator } from "react-native";
-import HeaderLogout from "../components/HeaderLogout";
-import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
-export default function PassengerOrderOnMap() {
+import { StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
+import { GOOGLE_MAPS_API_KEY } from "@env";
+const origin = { latitude: 37.78899, longitude: -122.4324 };
+const destination = { latitude: 37.78935, longitude: -122.45 };
+
+export default function PassengerOrderOnMap () {
+  const latitude = (origin.latitude + destination.latitude) / 2;
+  const longitude = (origin.longitude + destination.longitude) / 2;
+
   return (
-    <View style={styles.container}>
-      <Text>Order on map</Text>
-    </View>
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude,
+        longitude,
+        latitudeDelta: Math.abs(origin.latitude - destination.latitude) + 0.02,
+        longitudeDelta:
+          Math.abs(origin.longitude - destination.longitude) + 0.02,
+      }}
+    >
+      <Marker coordinate={origin} title="Origin" />
+      <Marker coordinate={destination} title="Destination" />
+      <MapViewDirections
+        origin={origin}
+        destination={destination}
+        apikey={GOOGLE_MAPS_API_KEY}
+      />
+    </MapView>
   );
-}
+};
+
 const styles = StyleSheet.create({
-  container: {
+  map: {
     flex: 1,
-    justifyContent: "center",
   },
 });
