86384601898fa61033284ebceab5d3c956f4cc03
LidorDan <lidordanon30@cs.colman.ac.il>
2023-06-07 19:50:33 +0300

    design driver profile

--- src/pages/DriverProfilePage.js
+++ src/pages/DriverProfilePage.js
@@ -2,7 +2,15 @@ import Icon from "react-native-vector-icons/FontAwesome5";
 // import Icon from "react-native-vector-icons/Feather";
 
 import React from "react";
-import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
 import { Button } from "react-native-paper";
 import UserAvatar from "react-native-user-avatar";
 import { useNavigation } from "@react-navigation/native";
@@ -24,81 +32,85 @@ export default function DriverProfilePage(props) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.color}>
-        <Text></Text>
-      </View>
-      <View style={{ alignItems: "center" }}>
-        <TouchableOpacity>
-          {imageProfile ? (
-            <Image source={{ uri: imageProfile }} style={styles.avatar} />
-          ) : (
-            <UserAvatar
-              size={110}
-              name={fullName}
-              style={styles.avatar}
-              textColor={"#061848"}
+      <ImageBackground
+        source={require("../assets/profilePage.png")}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <View style={styles.container}>
+          <View style={{ alignItems: "center" }}>
+            <TouchableOpacity>
+              {imageProfile ? (
+                <Image source={{ uri: imageProfile }} style={styles.avatar} />
+              ) : (
+                <UserAvatar
+                  size={110}
+                  name={fullName}
+                  style={styles.avatar}
+                  textColor={"#061848"}
+                />
+              )}
+            </TouchableOpacity>
+            {forOrder === "false" && (
+              <Icon
+                name="edit"
+                size={20}
+                style={styles.edit_icon}
+                onPress={() => {
+                  navigation.navigate("EditDriver", {
+                    fullName,
+                    phoneNumber,
+                    email,
+                    carModel,
+                    plateNumber,
+                    password,
+                    carColor,
+                    imageProfile,
+                  });
+                }}
+              ></Icon>
+            )}
+          </View>
+          <Text style={styles.driver_name}>{fullName} </Text>
+          <View style={styles.data_icons_Container}>
+            <Icon
+              name="envelope"
+              size={30}
+              color="#608cd7"
+              style={styles.email_icon}
             />
-          )}
-        </TouchableOpacity>
-        {forOrder === "false" && (
-          <Icon
-            name="edit"
-            size={20}
-            style={styles.edit_icon}
-            onPress={() => {
-              navigation.navigate("EditDriver", {
-                fullName,
-                phoneNumber,
-                email,
-                carModel,
-                plateNumber,
-                password,
-                carColor,
-                imageProfile,
-              });
-            }}
-          ></Icon>
-        )}
-      </View>
-      <Text style={styles.driver_name}>{fullName} </Text>
-      <View style={styles.data_icons_Container}>
-        <Icon
-          name="envelope"
-          size={30}
-          color="#608cd7"
-          style={styles.email_icon}
-        />
-        <Text style={styles.driver_email}>{email} </Text>
-      </View>
-      <View style={styles.data_icons_Container}>
-        <Icon
-          name="phone"
-          size={20}
-          color="#608cd7"
-          style={styles.phone_icon}
-        />
-        <Text style={styles.driver_phone}>{phoneNumber} </Text>
-      </View>
+            <Text style={styles.driver_email}>{email} </Text>
+          </View>
+          <View style={styles.data_icons_Container}>
+            <Icon
+              name="phone"
+              size={20}
+              color="#608cd7"
+              style={styles.phone_icon}
+            />
+            <Text style={styles.driver_phone}>{phoneNumber} </Text>
+          </View>
 
-      <Text style={styles.driver_carModel}>
-        My car : {carModel} , Car Number: {plateNumber}
-      </Text>
-      {/* <Text style={styles.driver_plateNumber}>Car Number: {plateNumber} </Text> */}
-      {/* <View style={styles.review_list}></View> */}
-      <View>
-        {forOrder === "false" && (
-          <Button
-            style={styles.sub_btn}
-            mode="contained"
-            buttonColor="#111"
-            onPress={() => {
-              navigation.navigate("Subscription");
-            }}
-          >
-            Subscription
-          </Button>
-        )}
-      </View>
+          <Text style={styles.driver_carModel}>My car : {carModel}</Text>
+          <Text style={styles.driver_carModel}>Car Number: {plateNumber} </Text>
+          {/* <Text style={styles.driver_plateNumber}>Car Number: {plateNumber} </Text> */}
+          {/* <View style={styles.review_list}></View> */}
+          <View>
+            {forOrder === "false" && (
+              <Button
+                style={styles.sub_btn}
+                mode="contained"
+                buttonColor="#111"
+                onPress={() => {
+                  navigation.navigate("Subscription");
+                }}
+              >
+                <Text style={styles.sub_text}>Subscription</Text>
+              </Button>
+            )}
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -108,25 +120,20 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  color: {
-    padding: 20,
-    width: "100%",
-    backgroundColor: "#5F84A2",
-    height: 130,
-  },
   avatar: {
-    width: 130,
-    height: 130,
+    width: 125,
+    height: 125,
     borderRadius: 100,
-    marginTop: -70,
-    backgroundColor: "#91AEC4",
+    marginTop: 102.5,
+    marginLeft: 9,
+    backgroundColor: "white",
   },
   driver_name: {
-    color: "black",
-    fontSize: 22,
-    marginLeft: 0,
-    marginTop: 20,
-    width: 300,
+    color: "white",
+    fontSize: 24,
+    marginLeft: 10,
+    marginTop: 50,
+    width: 400,
     textAlign: "center",
     fontWeight: "600",
   },
@@ -136,24 +143,28 @@ const styles = StyleSheet.create({
     marginBottom: 0,
     marginRight: 30,
   },
-  email_icon: { marginLeft: -10, marginRight: 20 },
-  phone_icon: { transform: [{ rotate: "95deg" }], marginTop: -5 },
+  email_icon: { marginLeft: -10, marginRight: 20, color: "#76A6ED" },
+  phone_icon: {
+    transform: [{ rotate: "95deg" }],
+    marginTop: -5,
+    color: "#76A6ED",
+  },
   driver_phone: {
-    color: "black",
+    color: "white",
     fontSize: 20,
-    marginLeft: 20,
-    marginTop: 4,
-    marginBottom: 10,
+    marginLeft: 15,
+    marginTop: 1,
+    marginBottom: 5,
   },
   driver_email: {
-    color: "#626FB4",
+    color: "white",
     fontSize: 20,
     marginLeft: 0,
     marginTop: 4,
     marginBottom: 7,
   },
   driver_carModel: {
-    color: "black",
+    color: "white",
     fontSize: 20,
     marginRight: 0,
     marginTop: 4,
@@ -170,15 +181,7 @@ const styles = StyleSheet.create({
     padding: 5,
     marginLeft: 350,
     marginTop: -50,
-  },
-  review_list: {
-    backgroundColor: "#000",
-    padding: 10,
-    width: "80%",
-    backgroundColor: "#5F84A2",
-    height: 130,
-    marginTop: 15,
-    marginBottom: 15,
+    color: "#76A6ED",
   },
   sub_btn: {
     justifyContent: "center",
@@ -186,5 +189,12 @@ const styles = StyleSheet.create({
     marginLeft: 50,
     marginRight: 50,
     width: "50%",
+    backgroundColor: "#76A6ED",
+    marginTop: 20,
   },
+  image: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  sub_text: { fontSize: 20, color: "#061848", fontWeight: "bold" },
 });
